533e000181ef58bade1335e4d68b99b500001d0f
Samuel Roach <samuelroach.2000@gmail.com>
2022-06-01 18:30:18 +0100

    Move all classes into __init__ to be accessed from ergast_py

--- ergast_py/__init__.py
+++ ergast_py/__init__.py
@@ -1,1 +1,25 @@
+from ergast_py.models.average_speed import AverageSpeed
+from ergast_py.models.constructor_standing import ConstructorStanding
+from ergast_py.models.driver import Driver
+from ergast_py.models.driver_standing import DriverStanding
+from ergast_py.models.fastest_lap import FastestLap
+from ergast_py.models.lap import Lap
+from ergast_py.models.location import Location
+from ergast_py.models.circuit import Circuit
+from ergast_py.models.constructor import Constructor
+from ergast_py.models.pit_stop import PitStop
+from ergast_py.models.race import Race
+from ergast_py.models.result import Result
+from ergast_py.helpers import Helpers
+from ergast_py.constants.status_type import StatusType
+from ergast_py.models.season import Season
+from ergast_py.models.standings_list import StandingsList
+from ergast_py.models.status import Status
+from ergast_py.models.timing import Timing
+from ergast_py.constants.expected import Expected
+from ergast_py.requester import Requester
+from ergast_py.type_constructor import TypeConstructor
+from ergast_py.ergast import Ergast
+
+
 __version__ = '0.1.0'
