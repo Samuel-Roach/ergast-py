fb792337cff29302168b40793f80c6c5989c2166
Samuel Roach <samuelroach.2000@gmail.com>
2022-06-07 00:11:37 +0100

    Add test definitions for type_constructor

--- tests/test_type_constructor.py
+++ tests/test_type_constructor.py
@@ -6,3 +6,90 @@ class TestTypeConstructor():
     """
 
     t = ergast_py.TypeConstructor()
+
+    def test_construct_location(self):
+        pass
+
+    def test_construct_circuit(self):
+        pass
+
+    def test_construct_circuits(self):
+        pass
+
+    def test_construct_constructor(self):
+        pass
+
+    def test_construct_constructors(self):
+        pass
+
+    def test_construct_driver(self):
+        pass
+
+    def test_construct_drivers(self):
+        pass
+
+    def test_construct_race(self):
+        pass
+
+    def test_construct_races(self):
+        pass
+
+    def test_construct_result(self):
+        pass
+
+    def test_construct_results(self):
+        pass
+
+    def test_construct_fastest_lap(self):
+        pass
+
+    def test_construct_average_speed(self):
+        pass
+
+    def test_construct_pit_stop(self):
+        pass
+
+    def test_construct_pit_stops(self):
+        pass
+
+    def test_construct_lap(self):
+        pass
+
+    def test_construct_laps(self):
+        pass
+
+    def test_construct_timing(self):
+        pass
+
+    def test_construct_timings(self):
+        pass
+
+    def test_construct_season(self):
+        pass
+
+    def test_construct_seasons(self):
+        pass
+
+    def test_construct_status(self):
+        pass
+
+    def test_construct_statuses(self):
+        pass
+
+    def test_construct_driver_standing(self):
+        pass
+
+    def test_construct_driver_standings(self):
+        pass
+
+    def test_construct_constructor_standing(self):
+        pass
+
+    def test_construct_constructor_standings(self):
+        pass
+
+    def test_construct_standings_list(self):
+        pass
+
+    def test_construct_standings_lists(self):
+        pass
