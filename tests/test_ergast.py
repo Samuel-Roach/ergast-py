1a6f238012b349d859670f25dcb1b8cdd59f9c94
Samuel Roach <samuelroach.2000@gmail.com>
2022-06-12 11:22:04 +0100

    Added function definition for ergast tests

--- tests/test_ergast.py
+++ tests/test_ergast.py
@@ -6,3 +6,18 @@ class TestErgast():
     """
 
     e = ergast_py.Ergast()
+
+    def test_filters_add(self):
+        # Test that when I add all the filters the params has them all
+        pass
+
+    def test_paging(self):
+        # Test that paging works when querying large data amounts
+        pass
+
+    def test_reset(self):
+        # Test that when I've run a query the params are reset
+        pass
+
+    def test_full(self):
+        pass
