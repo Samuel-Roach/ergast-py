2ca48b02b3ac3b3faeaa895faeb80e1f6ca6fc7a
Roger Esteve <rogeresteve30@gmail.com>
2023-01-21 19:53:43 +0100

    Fixed issue #4  when races didn't have race time

--- ergast_py/helpers.py
+++ ergast_py/helpers.py
@@ -14,6 +14,10 @@ class Helpers:
 
         Looking for the format of ``%Y-%m-%d %H:%M:%SZ``
         """
+        
+        if not time.strip():
+            time = "00:00:00Z"
+
         new_datetime = datetime.datetime.strptime(f"{date} {time}", "%Y-%m-%d %H:%M:%SZ")
         new_datetime = new_datetime.replace(tzinfo=datetime.timezone.utc)
         return new_datetime
