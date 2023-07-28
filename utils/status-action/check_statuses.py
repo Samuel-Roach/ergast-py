ab29bff08300acdf31061c0e2bc64ffb10bf281d
Samuel Roach <samuelroach.2000@gmail.com>
2023-07-28 12:11:21 +0200

    fix: pass missing_statuses around as a local

--- utils/status-action/check_statuses.py
+++ utils/status-action/check_statuses.py
@@ -7,22 +7,22 @@ import requests
 
 API_URL = "https://ergast.com/api/f1/status.json?limit=2000"
 CURRENT_STATUSES_PATH = "./utils/status-action/current_statuses.json"
-MISSING_STATUSES = []
 
 
 def generate_delimiter():
     return "".join(random.choices(string.ascii_lowercase + string.digits, k=5))
 
 
-def generate_output_string():
+def generate_output_string(status_list):
     output = ""
-    for status in MISSING_STATUSES:
-        output += f'- { status["statusId"] } ({status["status"]})\n'
+    for status in status_list:
+        output += f'- { status["statusId"] } ({ status["status"] })\n'
 
     return output
 
 
 def main():
+    missing_statuses = []
     response = requests.get(API_URL).json()["MRData"]["StatusTable"]
 
     with open(CURRENT_STATUSES_PATH, "r") as status_file:
@@ -31,17 +31,17 @@ def main():
     if current != response:
         for status in response["Status"]:
             if status not in current:
-                MISSING_STATUSES += status
+                missing_statuses += status
 
-    if len(MISSING_STATUSES) > 0:
+    if len(missing_statuses) > 0:
         delim = generate_delimiter()
         with open(os.environ["GITHUB_ENV"], "a") as environment_file:
             environment_file.write(f"MISSING_STATUSES<<{delim}")
-            environment_file.write(generate_output_string())
+            environment_file.write(generate_output_string(missing_statuses))
             environment_file.write(delim)
 
     with open(os.environ["GITHUB_OUTPUT"], "a") as environment_file:
-        environment_file.write(f"MISSING_STATUSES_COUNT={len(MISSING_STATUSES)}")
+        environment_file.write(f"MISSING_STATUSES_COUNT={len(missing_statuses)}")
 
 
 if __name__ == "__main__":
